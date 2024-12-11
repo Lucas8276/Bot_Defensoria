b2e567e883f5822a76b22561ec185ea876f04c38
Lucas8276 <lucasnahuquin@gmail.com>
2024-12-11 14:46:28 -0300

    Borrando credenciales

--- doc_sheet.js
+++
@@ -1,4 +0,0 @@
-    const SPREADSHEET_ID = "1eJvg2uRvGh6TktaSYxMrw6Zrv6LehK-pVtCezEjlQOM";
-    const WEBHOOK_USERNAME= "defensoriadelosvecinosctes";
-    const WEBHOOK_PASSWORD= "pruebav53@";
-    module.exports = { SPREADSHEET_ID , WEBHOOK_USERNAME, WEBHOOK_PASSWORD };
